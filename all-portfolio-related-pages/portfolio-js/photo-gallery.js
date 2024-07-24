3ad0fc8c7e1a47e08b54cd8035f6df4eaf9246e8
AidanNgo <aidanngo88@gmail.com>
2024-07-24 18:15:14 -0500

    just finished with the graduation photo gallery

---
+++ all-portfolio-related-pages/portfolio-js/photo-gallery.js
@@ -0,0 +1,56 @@
+//code for the functional lightbox
+const allImages = document.querySelectorAll("[data-index]");
+var lightbox = document.querySelector(".lightbox");
+var currentImage = document.getElementById("currentImage");
+var lightboxOn = 0;
+var imageIndex = 0;
+
+function closeLightbox(){
+    lightboxOn = 0;
+    lightbox.style.display = "none";
+}
+
+function plusImages(n){
+    openImage(imageIndex += n);
+}
+
+function openImage(n){
+    imageIndex = n;
+    if (lightboxOn == 0){
+        lightbox.style.display = "flex";
+        lightboxOn = 1;
+    }
+    if (n >= allImages.length){
+        imageIndex = 0;
+    }
+    if (n < 0){
+        imageIndex = allImages.length - 1;
+    }
+    currentImage.src = allImages[imageIndex].src;
+}
+
+allImages.forEach((img, index) => {
+    img.addEventListener('click', () => {
+        openImage(index);
+    });
+});
+
+//subtle scroll animation that allows elements to fade in
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        } else{
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el)); 
+
+// diables right click on the entire html page
+document.addEventListener('contextmenu', function(event) {
+    event.preventDefault();
+});
