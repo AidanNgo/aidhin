79360d6093f8f394ac78d1ed55318eb0185d74a3
AidanNgo <aidanngo88@gmail.com>
2024-08-09 14:03:56 -0500

    test 103

--- all-portfolio-related-pages/photo-gallery.js
+++ all-portfolio-related-pages/photo-gallery.js
@@ -74,17 +74,17 @@ document.addEventListener('contextmenu', function (event) {
   });
   
   // Optionally, prevent touch and hold actions
-  document.addEventListener('touchstart', function (event) {
-    if (event.touches.length > 1) {
-      event.preventDefault();
-    }
-  }, { passive: false });
+  // document.addEventListener('touchstart', function (event) {
+  //   if (event.touches.length > 1) {
+  //     event.preventDefault();
+  //   }
+  // }, { passive: false });
   
-  document.addEventListener('touchend', function (event) {
-    if (event.touches.length > 1) {
-      event.preventDefault();
-    }
-  }, { passive: false });
+  // document.addEventListener('touchend', function (event) {
+  //   if (event.touches.length > 1) {
+  //     event.preventDefault();
+  //   }
+  // }, { passive: false });
   
   // document.addEventListener('touchmove', function (event) {
   //   if (event.touches.length > 1) {
