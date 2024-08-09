1c661999392943c5eae72b6ad4a2c5a516102196
AidanNgo <aidanngo88@gmail.com>
2024-08-09 00:33:07 -0500

    still making tweaks to the whole iphone long-press thingy

--- all-portfolio-related-pages/photo-gallery.js
+++ all-portfolio-related-pages/photo-gallery.js
@@ -78,25 +78,16 @@ document.addEventListener('contextmenu', function (event) {
     if (event.touches.length > 1) {
       event.preventDefault();
     }
-    if (event.target.tagName === 'IMG') {
-      event.preventDefault();
-    }
   }, { passive: false });
   
   document.addEventListener('touchend', function (event) {
     if (event.touches.length > 1) {
       event.preventDefault();
     }
-    if (event.target.tagName === 'IMG') {
-      event.preventDefault();
-    }
   }, { passive: false });
   
   document.addEventListener('touchmove', function (event) {
     if (event.touches.length > 1) {
-      event.preventDefault();
-    }
-    if (event.target.tagName === 'IMG') {
       event.preventDefault();
     }
   }, { passive: false });
